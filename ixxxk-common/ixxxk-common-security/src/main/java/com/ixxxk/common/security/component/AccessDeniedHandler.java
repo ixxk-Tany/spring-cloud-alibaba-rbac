0636d2a2e03e420099a8e0ffa550fd11de74318d
5824519@qq.com <t5824519>
2020-08-13 11:50:13 +0800

    className

--- ixxxk-common/ixxxk-common-security/src/main/java/com/ixxxk/common/security/component/AccessDeniedHandler.java
+++ ixxxk-common/ixxxk-common-security/src/main/java/com/ixxxk/common/security/component/AccessDeniedHandler.java
@@ -40,7 +40,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 
 /**
- * @author CQTany 授权拒绝处理器，覆盖默认的OAuth2AccessDeniedHandler 包装失败信息到PigDeniedException
+ * @author CQTany 授权拒绝处理器，覆盖默认的OAuth2AccessDeniedHandler 包装失败信息到DeniedException
  */
 @Slf4j
 @Component
